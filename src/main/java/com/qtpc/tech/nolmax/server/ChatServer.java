d77efc839315c52744cb2fa3132d2a5aa8b899d8
QuanTrieuPCYT <admin@qtpc.tech>
2026-04-04 18:42:47 +0700

    Makes use of `io_uring`, `epoll` and `kqueue` whenever possible

--- src/main/java/com/qtpc/tech/nolmax/server/ChatServer.java
+++ src/main/java/com/qtpc/tech/nolmax/server/ChatServer.java
@@ -9,9 +9,20 @@ import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelPipeline;
 import io.netty.channel.EventLoopGroup;
+import io.netty.channel.MultiThreadIoEventLoopGroup;
+import io.netty.channel.epoll.Epoll;
+import io.netty.channel.epoll.EpollEventLoopGroup;
+import io.netty.channel.epoll.EpollServerSocketChannel;
+import io.netty.channel.kqueue.KQueue;
+import io.netty.channel.kqueue.KQueueEventLoopGroup;
+import io.netty.channel.kqueue.KQueueServerSocketChannel;
 import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.ServerSocketChannel;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.uring.IoUring;
+import io.netty.channel.uring.IoUringIoHandler;
+import io.netty.channel.uring.IoUringServerSocketChannel;
 import io.netty.handler.codec.protobuf.ProtobufDecoder;
 import io.netty.handler.codec.protobuf.ProtobufEncoder;
 import io.netty.handler.codec.protobuf.ProtobufVarint32FrameDecoder;
@@ -27,14 +38,38 @@ public class ChatServer {
         this.config = config;
     }
 
+    @SuppressWarnings("deprecation") // YES i know what i am doing
     public void start() {
-        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
-        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        EventLoopGroup bossGroup;
+        EventLoopGroup workerGroup;
+        Class<? extends ServerSocketChannel> channelClass;
+
+        if (IoUring.isAvailable()) {
+            log.info("Detected that io_uring syscall interface is available. You should be on Linux.");
+            bossGroup = new MultiThreadIoEventLoopGroup(1, IoUringIoHandler.newFactory());
+            workerGroup = new MultiThreadIoEventLoopGroup(IoUringIoHandler.newFactory());
+            channelClass = IoUringServerSocketChannel.class;
+        } else if (Epoll.isAvailable()) {
+            log.info("Detected that epoll syscall interface is available. You should be on Linux.");
+            bossGroup = new EpollEventLoopGroup(1);
+            workerGroup = new EpollEventLoopGroup();
+            channelClass = EpollServerSocketChannel.class;
+        } else if (KQueue.isAvailable()) {
+            log.info("Detected that kqueue syscall interface is available. You should be on *BSD or Darwin (Apple *OS).");
+            bossGroup = new KQueueEventLoopGroup(1);
+            workerGroup = new KQueueEventLoopGroup();
+            channelClass = KQueueServerSocketChannel.class;
+        } else {
+            log.info("Performant OS-specific I/O interface is not detected. Using Java's Non-blocking I/O interface...");
+            bossGroup = new NioEventLoopGroup(1);
+            workerGroup = new NioEventLoopGroup();
+            channelClass = NioServerSocketChannel.class;
+        }
 
         try {
             ServerBootstrap b = new ServerBootstrap();
             b.group(bossGroup, workerGroup)
-             .channel(NioServerSocketChannel.class)
+             .channel(channelClass)
              .childHandler(new ChannelInitializer<SocketChannel>() {
                  @Override
                  protected void initChannel(SocketChannel ch) {
